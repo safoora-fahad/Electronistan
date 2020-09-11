1c3eb53c75525348a4aa6d5206768808b4911914
Safoora Fahad <64247317+safoora-fahad@users.noreply.github.com>
2020-09-11 23:00:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+{
+  "Home Electronics"; [
+    {
+      "name": "Tv and Lcd",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 24,
+      "price": 50000
+    },
+     {
+      "name": "Refrigerator",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 24,
+      "price": 93000
+    },
+     {
+      "name": "Washing Machine",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 55,
+      "price": 35000
+    },
+    {
+       "name": "Air Conditioners",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 116,
+      "price": 68000
+    },
+  ]
+}
+
+{
+  "Digital Electronics"; [
+    {
+      "name": "Mobiles",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 345,
+      "price": 74000
+    },
+     {
+      "name": "Laptops",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 226,
+      "price": 42500
+    },
+     {
+      "name": "Tablets",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 79,
+      "price": 37000
+    },
+    {
+       "name": "Computers",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 35,
+      "price": 25000
+    },
+  ]
+}
+
+{
+  "Electronic Appliances"; [
+    {
+      "name": "Microwave",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 63,
+      "price": 11500
+    },
+     {
+      "name": "Kettle",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 80,
+      "price": 6000
+    },
+    {
+        "name": "Dishwasher",
+       "imageUrl": "https://dummyimage.com/556x985",
+       "review": 190,
+      "price": 19000
+    }, 
+  ]
+}
